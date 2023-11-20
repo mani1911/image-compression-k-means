864db89b65f56b809c8db280212e57020f94abfe
mani1911 <mani1911@pop-os.localdomain>
2023-11-21 01:54:06 +0530

    feat: dataset preparation

--- helpers.py
+++ helpers.py
@@ -2,55 +2,47 @@ import sys
 from skimage import io
 from sklearn.cluster import KMeans
 import numpy as np
-import matplotlib.pyplot as plt
+import os
+import shutil
 
-filename = sys.argv[1]
+from kneed import KneeLocator, DataGenerator
 
-# Y = [95404286.98810115, 187264628.1696772, 277250438.35128206, 363357797.3740872, 449195469.93302774, 534162601.46219414, 611476493.6256964, 694383401.9162117, 768040874.6659051]
-# X =  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+dataset_path = 'dataset/'
+output_path = 'output/'
 
-Y = []
-X = []
+X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+Y = [7661231898.168653, 3290707373.3764653, 2266018605.274069, 1512373450.479423, 1196348509.879937, 966578851.923748, 822065746.9691927, 721009283.7247559, 627830528.0580585, 566890254.0646206, 520692339.94309807]
 
-# reading the image
-image = io.imread(filename)
-rows, cols = image.shape[0], image.shape[1]
-img_original = image
-image = image.reshape(rows * cols, 3)
-points = 64 * 64 * 3
-
-def findInertia(image, centroids):
-    inertia = 0
-    for i in range(640):
-         for j in range(640):
-              if image[i][j] not in centroids:
-                   for c in centroids:
-                        inertia += np.linalg.norm(image[i][j] - c)
-    return inertia
-
-def findKMeans(image):
-
-    for i in range(1,10):
+# Find optimal k value by plotting elbow curve 
+def FindKFromImage(imagePath):
+    image = io.imread(imagePath)
+    rows, cols = image.shape[0], image.shape[1]
+    image = image.reshape(rows * cols, 3)
+    X = []
+    Y = []
+    for i in range(1, 12):  
         kMeans = KMeans(n_clusters = i)
         kMeans.fit(image)
-        centers = np.asarray(kMeans.cluster_centers_, dtype=np.uint8)
-
-        # print(centers)
-
-        inertia =  findInertia(image=img_original, centroids=centers)
-
-        Y.append(inertia / points)
         X.append(i)
+        Y.append(kMeans.inertia_)
+    kneedle = KneeLocator(X, Y, curve='convex', direction='decreasing')
+    optimal_k = kneedle.elbow
+    return optimal_k
 
-def plot(nums1, nums2):
-    plt.plot(nums1, nums2)
-    plt.savefig('test')
-     
+# create the dataset for the model
+def CreateDataset():
+    os.makedirs('output', exist_ok=True)
+    for filename in os.listdir(dataset_path):
+        file_path = os.path.join(dataset_path, filename)
+        if os.path.isfile(file_path) and filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            elbowK = str(FindKFromImage(file_path))
+            os.makedirs(f'output/{elbowK}', exist_ok=True)
 
-findKMeans(image)
-print(X, Y)
-# print(X, Y)
-# plot(X, Y)
-
+            destination_path = os.path.join(output_path + elbowK, filename)
+            shutil.copy(file_path, destination_path)
 
     
+
+
+# FindKFromImage('dataset/me.jpeg')
+CreateDataset()
